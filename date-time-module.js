537d8a80285a68b69f2b25b1ac6ca0dd4555ecc4
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-27 16:11:29 +0200

    Add export and use my date time module

---
+++ date-time-module.js
@@ -0,0 +1,6 @@
+// export the current date and time as module
+'use strict';
+
+exports.myDateTime = function () {
+    return Date();
+  };
---
+++ index-date-time.js
@@ -0,0 +1,8 @@
+// Import own module
+
+'use strict';
+
+// import my date-time-module.js module
+const dateTime = require('./date-time-module');
+
+console.log(`Current time is: ${dateTime.myDateTime()}`);
